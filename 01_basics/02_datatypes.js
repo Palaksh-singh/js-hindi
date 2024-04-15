33dee9a5046b6c6d74a46014694fb633716d01cd
Palaksh-singh <167078995+Palaksh-singh@users.noreply.github.com>
2024-04-15 11:22:49 +0000

    config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+console.log("Palaksh")
+
+let name = "Palaksh"
+let age = 19
+let isLoggedIn = true
+let state = null
+
+
+// primitive datatypes
+
+   // number => 2 to power 53
+   // bigint 
+   // String => ""
+   // boolean => true or false , ha ya naa
+   // null
+   // undefined =>
+   // symbol => unique
+
+// object
+
+
+console.log(typeof undefined);
+console.log(typeof null);
