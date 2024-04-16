d2cc5a762988720d2301485373548f6e4b5cb962
Palaksh-singh <167078995+Palaksh-singh@users.noreply.github.com>
2024-04-16 06:16:32 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// we should avoid this type of coding 
+
+
+
+// strict check is triple equal sign  ===
+
+console.log("2" == 2);
+console.log("2" === 2);
